4a6b2504fa2858731ceb5d3ceb583d13fb3be9a7
Martin Santamaria <msantamaria@mvdstudio.com.uy>
2024-12-20 10:00:45 -0300

    Fixeos sobre la marcha

--- resources/views/content/e-commerce/front/layouts/ecommerce-layout.blade.php
+++ resources/views/content/e-commerce/front/layouts/ecommerce-layout.blade.php
@@ -16,6 +16,15 @@ $customizerHidden = ($customizerHidden ?? '');
 @section('layoutContent')
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP51PKQZTS"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-KP51PKQZTS');
+  </script>
 
 
 @include('content/e-commerce/front/layouts/navbar')
